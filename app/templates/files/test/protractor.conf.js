5f8d3e4a4a8f3402d22683254115a0d7f736e3c1
Josef Engelfrost <lists@josf.se>
2015-10-09 14:01:34 +0200

    Update selenium server

--- app/templates/files/test/protractor.conf.js
+++ app/templates/files/test/protractor.conf.js
@@ -1,5 +1,5 @@
 /* global exports */
 exports.config = {
-  seleniumServerJar: './../node_modules/gulp-protractor/node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
+  seleniumServerJar: './../node_modules/gulp-protractor/node_modules/protractor/selenium/selenium-server-standalone-2.47.1.jar',
   specs: ['protractor/*.js']
 };
